22f4f79f28f64319513ba965bdb7ea7bca8fc2c8
Sargam Poudel <sargampoudel100@gmail.com>
2024-11-21 13:02:17 +0530

    feat: add code to replicate

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,85 @@
-import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
+export default async function Home() {
+  const loginRes = await LoginToAppX("youremail", "yourpassword");
+  console.log(loginRes);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className="mx-auto max-w-screen-xl py-6">
+      <AppxVideoPlayer course_id="14" video_id="3006" />
+    </main>
   );
+}
+
+export async function AppxVideoPlayer({
+  course_id,
+  video_id,
+}: {
+  course_id: string;
+  video_id: string;
+}) {
+  // Get this from the appx api
+  const auth_token = "";
+  const url = `https://harkiratapi.classx.co.in/get/fetchVideoDetailsById?course_id=${course_id}&video_id=${video_id}&ytflag=${1}&folder_wise_course=${1}`;
+
+  const config = {
+    url,
+    method: "get",
+    maxBodyLength: Infinity,
+    headers: {
+      Authorization: auth_token,
+      "Auth-Key": "appxapi",
+      "User-Id": "43916",
+    },
+  };
+  const res = await axios.request(config);
+  const videoUrl = res.data.data.video_player_url;
+  const token = res.data.data.video_player_token;
+  const username = "";
+  const email = "";
+  const url1 = `${videoUrl}${token}&watermark=${username}%0A${email}`;
+  console.log(url1);
+
+  return (
+    <iframe
+      src={`${videoUrl}${token}&watermark=${username}%0A${email}`}
+      className="w-[80vw] h-[80vh] rounded-lg"
+    ></iframe>
+  );
+}
+
+type LoginToAppXResponse = {
+  email: string;
+  phone: string;
+  token: string;
+};
+
+export async function LoginToAppX(
+  username: string,
+  password: string
+): Promise<LoginToAppXResponse> {
+  const loginUrl = "https://harkiratapi.classx.co.in/post/userLogin";
+  const formData = new FormData();
+  formData.append("phone", username);
+  formData.append("email", username);
+  formData.append("password", password);
+  const config = {
+    url: loginUrl,
+    method: "post",
+    headers: {
+      "Auth-Key": "appxapi",
+      //"Auth-Key": process.env.APPX_AUTH_KEY,
+    },
+    data: formData,
+  };
+  try {
+    const response = await axios.request(config);
+    return {
+      email: response.data.data.email,
+      phone: response.data.data.phone,
+      token: response.data.data.token,
+    };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
